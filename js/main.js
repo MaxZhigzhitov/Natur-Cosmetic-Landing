ca13feac3f7733c4268194683e249b7ae72be1c6
MaxZhigzhitov <maxzhidesktop@gmail.com>
2023-02-21 22:38:42 +0800

    fixed background visibility and appearance of the first screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const activeRatings = document.getElementsByClassName("stars-active-wrapper");
 
 getRating(activeRatings);
@@ -15,3 +16,24 @@ function renderRating(ratings, index, ratingWidth) {
     ratings[index].style.width = ratingWidth + "%";
 }
 
+
+const modalContainer = document.querySelector('.modal-container');
+const modalOpen = document.querySelector('.modal-control');
+const modalClose = modalContainer.querySelector('.modal-close-icon');
+
+modalOpen.addEventListener('click', openModal)
+modalClose.addEventListener('click', closeModal);
+modalContainer.addEventListener('click', function(e){
+    if(e.target == modalContainer){
+        closeModal();
+    }
+});
+
+
+function openModal(){
+    modalContainer.classList.add('active');
+}
+
+function closeModal(){
+    modalContainer.classList.remove('active');
+}
